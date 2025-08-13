8e79b26164d654ded2ab22ed8c25aa2036809f62
송준현 <skyblizzard64@gmail.com>
2025-08-13 14:29:13 +0900

    adaface

--- top_k_eval_new.py
+++ top_k_eval_new.py
@@ -391,7 +391,6 @@ def main(args):
 
     backbone = torch.compile(backbone)
 
-
     ALL_PERSON_FOLDERS = sorted(os.listdir(args.data_path))
     NUM_FOLDER_TO_PROCESS = len(ALL_PERSON_FOLDERS) // args.split
     folders_to_process = ALL_PERSON_FOLDERS[:NUM_FOLDER_TO_PROCESS]
@@ -728,7 +727,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=512, help="임베딩 추출 시 배치 크기")
     parser.add_argument('--load_cache' , type=str , default = None ,help="임베딩 캐시경로")
     parser.add_argument('--save_cache' , action='store_true')
-    parser.add_argument('--split',default=4 , help='전체클래스수 / N ')
+    parser.add_argument('--split', type= int ,default=1 , help='전체클래스수 / N ')
     args = parser.parse_args()
 
     #args.data_path = '/home/ubuntu/KOR_DATA/kor_data_full_Middle_Resolution_aligend'
